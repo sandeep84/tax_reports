0d92c7ffbbba9cdf1fda418cca69b3d34d559a7d
Sandeep Vangipuram <sandeep84@gmail.com>
2022-01-08 17:00:48 +0000

    Implement reporting (partial) for capital gains

--- tax_report.py
+++ tax_report.py
@@ -4,6 +4,8 @@ import dateutil
 import pprint
 from jinja2 import Environment, PackageLoader, select_autoescape
 import babel.numbers
+from collections import deque
+import copy
 
 def insert_account_entry(account_entry, parent_account_entry, summary):
     if parent_account_entry is not None and account_entry['type'] == parent_account_entry['type'] and account_entry['currency'] == parent_account_entry['currency']:
@@ -16,9 +18,79 @@ def insert_account_entry(account_entry, parent_account_entry, summary):
         
         summary[account_entry['currency']][account_entry['type']].append(account_entry)
 
-def summarise_account(account, parent_account, root_currency, summary):
+def summarise_capital_gains(account, parent_account_entry, root_currency, summary):
+    account_entry = {
+        'guid': account.guid,
+        'name': account.name,
+        'type': account.type,
+        'currency': 'INR', #account.commodity.base_currency.mnemonic,
+        'value': 0,
+        'value_in_root_currency': 0,
+        'sub_total': 0,
+        'sub_total_in_root_currency': 0,
+        'splits': [],
+        'children': [],
+    }
+
+    units = deque()
+
+    for split in account.splits:
+        tr = split.transaction
+        #print(f'{tr.post_date}, {tr.description}, {split.quantity}, {split.value}')
+        if split.quantity >= 0: # A purchase or a dividend reinvestment
+            units.append({
+                'purchase_date': tr.post_date,
+                'purchase_value': split.value,
+                'purchase_rate': split.value / split.quantity,
+                'quantity': split.quantity,
+            })
+        else:
+            # A redemption
+            quantity = -split.quantity # The quantity to be removed from the oldest splits
+            while quantity > 0:
+                if units[0]['quantity'] > quantity:
+                    # The redemption is a part of the oldest purchase
+                    # Make a copy of the split for reporting
+                    redeemed_split = copy.deepcopy(units[0])
+                    redeemed_split['quantity'] = quantity
+                    redeemed_split['description'] = split.transaction.description
+                    redeemed_split['sale_date'] = split.transaction.post_date
+                    redeemed_split['sale_rate'] = split.value / split.quantity
+                    redeemed_split['sale_value'] = quantity * redeemed_split['sale_rate']
+                    redeemed_split['purchase_value'] = quantity * redeemed_split['purchase_rate']
+                    redeemed_split['capital_gains'] = redeemed_split['sale_value'] - redeemed_split['purchase_value']
+                    account_entry['value'] += redeemed_split['capital_gains']
+                    account_entry['splits'].append(redeemed_split)
+
+                    # And modify the original split to remove the redeemed quantity
+                    units[0]['quantity'] -= quantity
+                    units[0]['purchase_value'] = units[0]['quantity'] * redeemed_split['purchase_rate']
+
+                    quantity = 0
+                else:
+                    # The oldest purchase only covers part of the quantity sold
+                    # Just pop the original split for reporting
+                    redeemed_split = units.popleft()
+                    redeemed_split['description'] = split.transaction.description
+                    redeemed_split['sale_date'] = split.transaction.post_date
+                    redeemed_split['sale_rate'] = split.value / split.quantity
+                    redeemed_split['sale_value'] = redeemed_split['quantity'] * redeemed_split['sale_rate']
+                    redeemed_split['capital_gains'] = redeemed_split['sale_value'] - redeemed_split['purchase_value']
+                    account_entry['value'] += redeemed_split['capital_gains']
+                    account_entry['splits'].append(redeemed_split)
+
+                    quantity -= redeemed_split['quantity']
+
+    if len(account_entry['splits']) > 0:
+        insert_account_entry(account_entry, parent_account_entry, summary)
+
+    return account_entry
+
+def summarise_account(account, parent_account_entry, root_currency, summary):
     if account.type in ['BANK', 'CASH', 'LIABILITY', 'CREDIT']:
         return None
+    elif account.type in ['STOCK', 'MUTUAL']:
+        return summarise_capital_gains(account, parent_account_entry, root_currency, summary)
 
     account_entry = {
         'guid': account.guid,
@@ -69,7 +141,7 @@ def summarise_account(account, parent_account, root_currency, summary):
             account_entry['sub_total_in_root_currency'] += child_entry['sub_total_in_root_currency']
 
     if account_entry['sub_total'] != 0 or len(account_entry['children']) > 0:
-        insert_account_entry(account_entry, parent_account, summary)
+        insert_account_entry(account_entry, parent_account_entry, summary)
 
     return account_entry
 
@@ -91,7 +163,7 @@ if __name__ == '__main__':
 
     summary = {}
     summarise_account(book.root_account, None, book.default_currency, summary)
-    pprint.pprint(summary)
+    #pprint.pprint(summary)
 
     env = Environment(
         loader=PackageLoader("tax_report"),
